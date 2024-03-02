20887c954586e154b7fd1e8b5d1b8ce1b807c69d
minthukyaw <flutter.sama@gmail.com>
2024-03-02 13:30:16 +0630

    added .gitignore and make some changes

---
+++ README.md
@@ -0,0 +1,1 @@
+This project is to test git for Job Ready Java Batch 5 class
--- src/main/MainEntry.java
+++ src/main/MainEntry.java
@@ -4,6 +4,7 @@ public class MainEntry {
 
 	public static void main(String[] args) {
 		System.out.println("Application is started");
+		System.out.println("Make Some Changes...");
 	}
 
 }

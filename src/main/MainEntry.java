b4dc25418051f9b8b7a9d90f84afadc789fa1440
minthukyaw <flutter.sama@gmail.com>
2024-03-02 13:43:50 +0630

    changed editor and update changed MainEntry.java

--- src/main/MainEntry.java
+++ src/main/MainEntry.java
@@ -5,6 +5,7 @@ public class MainEntry {
 	public static void main(String[] args) {
 		System.out.println("Application is started");
 		System.out.println("Make Some Changes...");
+		System.out.println("Make Changed 2");
 	}
 
 }
